e9d53bf17a04952bb27ce796430946d43bb91e95
emthedm1 <emartin2@instructors.2u.com>
2022-05-10 23:00:39 -0600

    started building app.js page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState();
+
+  // const onSubmit = (event) => {
+  //   event.preventDefault()
+  //   const response = await fetch('s', {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({ text: textInput })
+  //   })
+  //   const data = await response.json()
+  //   setResult(data.result)
+  //   setText("")
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextareaAutosize
+        aria-label="minimum height"
+        minRows={12}
+        placeholder="Enter poetry here"
+        style={{ width: 800 }}
+      />
+      <Button variant="contained">Submit For Poetry</Button>
     </div>
   );
 }
