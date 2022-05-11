c721c42c775a47718b4d8c7788dbef8141729dcd
emthedm1 <emartin2@instructors.2u.com>
2022-05-10 23:27:08 -0600

    added response section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import Button from "@mui/material/Button";
+import {
+  Button,
+  Container,
+  FormGroup,
+  List,
+  ListItem,
+  ListItemText,
+  FormControlLabel,
+  Grid,
+  Typography,
+  Box,
+  ListItemIcon,
+} from "@mui/material/";
+import { styled } from "@mui/material/styles";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
 import { useState } from "react";
 import "./App.css";
 
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+}
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
 function App() {
   const [text, setText] = useState("");
   const [result, setResult] = useState();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
 
   // const onSubmit = (event) => {
   //   event.preventDefault()
@@ -25,13 +52,30 @@ function App() {
 
   return (
     <div className="App">
-      <TextareaAutosize
-        aria-label="minimum height"
-        minRows={12}
-        placeholder="Enter poetry here"
-        style={{ width: 800 }}
-      />
-      <Button variant="contained">Submit For Poetry</Button>
+      <Container maxWidth="sm">
+        <TextareaAutosize
+          aria-label="minimum height"
+          minRows={12}
+          placeholder="Enter poetry here"
+          style={{ width: 500 }}
+        />
+        <Button variant="contained">Submit For Poetry</Button>
+      </Container>
+      <Container maxWidth="sm">
+        <Demo>
+          <List dense={dense}>
+            {generate(
+              <ListItem>
+                <ListItemIcon></ListItemIcon>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+              </ListItem>
+            )}
+          </List>
+        </Demo>
+      </Container>
     </div>
   );
 }
